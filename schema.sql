8224961b0fed10ab2080098bccf7c7df1f1fe95a
Leah Brady <brady.leah@gmail.com>
2020-11-24 21:31:36 -0500

    add pared down ERD and schema

--- schema.sql
+++ schema.sql
@@ -1,49 +1,36 @@
-﻿CREATE TABLE "state" (
-    "state_ID" INTEGER   NOT NULL PRIMARY KEY,
-    "state" VARCHAR   NOT NULL
-);
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/4wVRHO
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-CREATE TABLE "county" (
-    "county_ID" INTEGER   NOT NULL PRIMARY KEY,
-    "county" VARCHAR   NOT NULL
-);
 
 CREATE TABLE "county_state" (
-    "county_ID" INTEGER   NOT NULL,
-    "state_ID" INTEGER   NOT NULL
-);
-
-CREATE TABLE "parties" (
-    "party_ID" INTEGER   NOT NULL PRIMARY KEY,
-    "party_name" VARCHAR   NOT NULL
+    "county_state_ID" INTEGER   NOT NULL,
+    "county_state_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_county_state" PRIMARY KEY (
+        "county_state_ID"
+     )
 );
 
 CREATE TABLE "candidates" (
-    "candidate_ID" INTEGER   NOT NULL PRIMARY KEY,
+    "candidate_ID" INTEGER   NOT NULL,
     "candidate_name" VARCHAR   NOT NULL,
-    "party_ID" INTEGER   NOT NULL
+    "party" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_candidates" PRIMARY KEY (
+        "candidate_ID"
+     )
 );
 
 CREATE TABLE "county_votes" (
-    "county_ID" INTEGER   NOT NULL,
+    "county_state_ID" INTEGER   NOT NULL,
     "candidate_ID" INTEGER   NOT NULL,
     "candidate_votes" INTEGER   NOT NULL,
+    "total_votes" INTEGER   NOT NULL,
     "year" INTEGER   NOT NULL
 );
 
-ALTER TABLE "county_state" ADD CONSTRAINT "fk_county_state_county_ID" FOREIGN KEY("county_ID")
-REFERENCES "county" ("county_ID");
-
-ALTER TABLE "county_state" ADD CONSTRAINT "fk_county_state_state_ID" FOREIGN KEY("state_ID")
-REFERENCES "state" ("state_ID");
-
-ALTER TABLE "candidates" ADD CONSTRAINT "fk_candidates_party_ID" FOREIGN KEY("party_ID")
-REFERENCES "parties" ("party_ID");
-
-ALTER TABLE "county_votes" ADD CONSTRAINT "fk_county_votes_county_ID" FOREIGN KEY("county_ID")
-REFERENCES "county" ("county_ID");
+ALTER TABLE "county_votes" ADD CONSTRAINT "fk_county_votes_county_state_ID" FOREIGN KEY("county_state_ID")
+REFERENCES "county_state" ("county_state_ID");
 
 ALTER TABLE "county_votes" ADD CONSTRAINT "fk_county_votes_candidate_ID" FOREIGN KEY("candidate_ID")
 REFERENCES "candidates" ("candidate_ID");
-
 
