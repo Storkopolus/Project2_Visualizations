e64c90f4deff34bc2d9f6d16a5dfa22edf0f7b3b
Leah Brady <brady.leah@gmail.com>
2020-11-24 21:41:54 -0500

    add party table back in

--- schema.sql
+++ schema.sql
@@ -11,10 +11,18 @@ CREATE TABLE "county_state" (
      )
 );
 
+CREATE TABLE "parties" (
+    "party_ID" INTEGER   NOT NULL,
+    "party_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_parties" PRIMARY KEY (
+        "party_ID"
+     )
+);
+
 CREATE TABLE "candidates" (
     "candidate_ID" INTEGER   NOT NULL,
     "candidate_name" VARCHAR   NOT NULL,
-    "party" VARCHAR   NOT NULL,
+    "party_ID" INTEGER   NOT NULL,
     CONSTRAINT "pk_candidates" PRIMARY KEY (
         "candidate_ID"
      )
@@ -27,6 +35,9 @@ CREATE TABLE "county_votes" (
     "total_votes" INTEGER   NOT NULL,
     "year" INTEGER   NOT NULL
 );
+
+ALTER TABLE "candidates" ADD CONSTRAINT "fk_candidates_party_ID" FOREIGN KEY("party_ID")
+REFERENCES "parties" ("party_ID");
 
 ALTER TABLE "county_votes" ADD CONSTRAINT "fk_county_votes_county_state_ID" FOREIGN KEY("county_state_ID")
 REFERENCES "county_state" ("county_state_ID");
