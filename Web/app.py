6188e8b50f014b992db3f4ee608821aca140c048
Jermaine <jtc0196@gmail.com>
2020-12-01 18:13:27 -0500

    file cleanup

--- Web/app.py
+++ Web/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-        return render_template('dashboard.html')
+        return render_template('index.html')
 
 if __name__=='__main__':
         app.run(debug=True)
