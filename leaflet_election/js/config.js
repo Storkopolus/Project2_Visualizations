5bd41e6534e5907a08dfc455f85f80dff439c47d
cristina0000 <cristina.sheridan0@gmail.com>
2020-12-01 20:51:13 -0500

    added push to postgres and beginning leaflet

---
+++ leaflet_election/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiY21waWV0cm8iLCJhIjoiY2thdHVxeDUyMHV4MTJ5cDN4YmtkbjgwOCJ9.TGFDOxNLoJpgdBxBtw6zJw";
---
+++ leaflet_election/js/logic3.js
@@ -0,0 +1,51 @@
+// Creating map object
+var myMap = L.map("map", {
+  center: [40.7128, -74.0059],
+  zoom: 11
+});
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Use this link to get the geojson data.
+var link = "static/data/nyc.geojson";
+
+// Function that will determine the color of a neighborhood based on the borough it belongs to
+function chooseColor(borough) {
+  switch (borough) {
+  case "Brooklyn":
+    return "yellow";
+  case "Bronx":
+    return "red";
+  case "Manhattan":
+    return "orange";
+  case "Queens":
+    return "green";
+  case "Staten Island":
+    return "purple";
+  default:
+    return "black";
+  }
+}
+
+// Grabbing our GeoJSON data..
+d3.json(link, function(data) {
+  // Creating a geoJSON layer with the retrieved data
+  L.geoJson(data, {
+    style: function(feature) {
+      return {
+        color: "white",
+        fillColor: chooseColor(feature.properties.borough),
+        fillOpacity: 0.5,
+        weight: 1.5
+      };
+    }
+  }).addTo(myMap);
+});
