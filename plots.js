b12487f31b9b4e49e86fa9c68ec007cfc6157aea
Ben Stork <58055482+Storkopolus@users.noreply.github.com>
2020-12-03 14:30:13 -0500

    Create plots.js
    Could not commit Leah's changes so copied this file over

---
+++ plots.js
@@ -0,0 +1,68 @@
+ lb_plots
+//set colors for use in pie charts
+var colors = ['rgb(165,0,38)', 'rgb(49,54,149)']
+//Add 2016 candidates and total votes
+var trace1 = {
+  labels: ["Hillary Clinton", "Donald Trump"],
+  values: [65844241,62979031],
+  type: 'pie'
+};
+//put trace info into a variable
+var data1 = [trace1];
+//set layout for first pie chart
+var layout1 = {
+  title: `2016 Votes `,
+  marker: {
+    color: colors
+  }
+};
+//add new pie chart to plot1 div tag
+Plotly.newPlot("plot1", data1, layout);
+//add 2020 candidates and total votes
+var trace2 = {
+    labels: ["Joe Biden", "Donald Trump"],
+    values: [79531835,73532410],
+    type: 'pie'
+  };
+  //put trace info into a variable
+  var data2 = [trace2];
+  //set layout for second pie chart
+  var layout2 = {
+    title: `2020 Votes`,
+    marker: {
+      color: colors
+    }
+  };
+//add new pie chart to plot2 div tag
+Plotly.newPlot("plot2", data2, layout);
+
+funciton init()
+
+var trace1 = {
+  labels: ["Hillary Clinton", "Donald Trump"],
+  values: [`${dem_votes}`,`${rep_votes}`],
+  type: 'pie'
+};
+
+var data = [trace1];
+
+var layout = {
+  title: `2016 Votes: ${county_state} `,
+};
+
+Plotly.newPlot("plot1", data, layout);
+
+var trace2 = {
+    labels: ["Joe Biden", "Donald Trump"],
+    values: [`${dem_votes}`,`${rep_votes}`],
+    type: 'pie'
+  };
+  
+  var data = [trace2];
+  
+  var layout = {
+    title: `2020 Votes: ${county_state}`,
+  };
+  
+  Plotly.newPlot("plot2", data, layout);
+ main
