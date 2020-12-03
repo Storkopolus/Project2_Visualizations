57bc72c3efc50c4418f60c5ab2f726f20b3a4043
Leah Brady <brady.leah@gmail.com>
2020-12-03 13:40:18 -0500

    add comments and minor fixes to second pie chart

--- plots.js
+++ plots.js
@@ -1,36 +1,36 @@
+//set colors for use in pie charts
 var colors = ['rgb(165,0,38)', 'rgb(49,54,149)']
-
+//Add 2016 candidates and total votes
 var trace1 = {
   labels: ["Hillary Clinton", "Donald Trump"],
   values: [65844241,62979031],
   type: 'pie'
 };
-
-var data = [trace1];
-
+//put trace info into a variable
+var data1 = [trace1];
+//set layout for first pie chart
 var layout1 = {
   title: `2016 Votes `,
   marker: {
     color: colors
   }
 };
-
-Plotly.newPlot("plot1", data, layout);
-
+//add new pie chart to plot1 div tag
+Plotly.newPlot("plot1", data1, layout);
+//add 2020 candidates and total votes
 var trace2 = {
     labels: ["Joe Biden", "Donald Trump"],
     values: [79531835,73532410],
     type: 'pie'
   };
-  
-  var data = [trace2];
-  
+  //put trace info into a variable
+  var data2 = [trace2];
+  //set layout for second pie chart
   var layout2 = {
     title: `2020 Votes`,
     marker: {
       color: colors
     }
   };
-
-  
-  Plotly.newPlot("plot2", data, layout);
+//add new pie chart to plot2 div tag
+Plotly.newPlot("plot2", data2, layout);
