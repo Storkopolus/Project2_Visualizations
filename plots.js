adacecbed479f4cc2df6ced1f6c46318d755e3ce
Leah Brady <brady.leah@gmail.com>
2020-12-03 13:23:45 -0500

    add total votes for each candidate

--- plots.js
+++ plots.js
@@ -1,28 +1,26 @@
-funciton init()
-
 var trace1 = {
   labels: ["Hillary Clinton", "Donald Trump"],
-  values: [`${dem_votes}`,`${rep_votes}`],
+  values: [65844241,62979031],
   type: 'pie'
 };
 
 var data = [trace1];
 
-var layout = {
-  title: `2016 Votes: ${county_state} `,
+var layout1 = {
+  title: `2016 Votes `,
 };
 
 Plotly.newPlot("plot1", data, layout);
 
 var trace2 = {
     labels: ["Joe Biden", "Donald Trump"],
-    values: [`${dem_votes}`,`${rep_votes}`],
+    values: [79531835,73532410],
     type: 'pie'
   };
   
   var data = [trace2];
   
-  var layout = {
+  var layout2 = {
     title: `2020 Votes: ${county_state}`,
   };
   
